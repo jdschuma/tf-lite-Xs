f503683615b3ebb13f29c562a5edb1f2f3a1dc03
Josh Schumacher <joshschumacher@dontdrivedirty.com>
2022-01-18 18:55:00 -0800

    cleanup req.txt file

--- train.py
+++ train.py
@@ -1,12 +1,5 @@
-import os
-import matplotlib.pyplot as plt
-import numpy as np
-
-from tflite_model_maker.config import ExportFormat, QuantizationConfig
 from tflite_model_maker import model_spec
 from tflite_model_maker import object_detector
-
-from tflite_support import metadata
 
 import tensorflow as tf
 assert tf.__version__.startswith('2')
